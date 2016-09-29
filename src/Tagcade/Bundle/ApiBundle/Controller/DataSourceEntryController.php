0f2943d4f03f884e8b21e867270961213e679ef7
Hieu Tran <dtag.dev1@gmail.com>
2016-09-29 10:23:13 +0700

    create service, handle for data source integration and controller for data source entry

---
+++ src/Tagcade/Bundle/ApiBundle/Controller/DataSourceEntryController.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Tagcade\Bundle\ApiBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\View\View;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Tagcade\Handler\HandlerInterface;
+use Tagcade\Model\Core\DataSourceEntryInterface;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+/**
+ * @Rest\RouteResource("datasourceentry")
+ */
+class DataSourceEntryController extends RestControllerAbstract implements ClassResourceInterface
+{
+    /**
+     * Get all data sources
+     *
+     * @Rest\View(serializerGroups={"datasource.detail", "dataSourceEntry.detail", "user.summary"})
+     *
+     * @ApiDoc(
+     *  section = "Data Source Entry",
+     *  resource = true,
+     *  statusCodes = {
+     *      200 = "Returned when successful"
+     *  }
+     * )
+     *
+     * @return DataSourceEntryInterface[]
+     */
+    public function cgetAction()
+    {
+        return $this->all();
+    }
+
+    /**
+     * Get a single data source entry for the given id
+     *
+     * @Rest\View(serializerGroups={"datasource.detail", "dataSourceEntry.detail", "user.summary"})
+     *
+     * @ApiDoc(
+     *  section = "Data Source",
+     *  resource = true,
+     *  statusCodes = {
+     *      200 = "Returned when successful"
+     *  }
+     * )
+     *
+     * @param int $id the resource id
+     *
+     * @return \Tagcade\Model\Core\DataSourceInterface
+     * @throws NotFoundHttpException when the resource does not exist
+     */
+    public function getAction($id)
+    {
+        return $this->one($id);
+    }
+
+    /**
+     * Create a data source entry from the submitted data
+     *
+     * @ApiDoc(
+     *  section = "Data Sources Entry",
+     *  resource = true,
+     *  statusCodes = {
+     *      200 = "Returned when successful",
+     *      400 = "Returned when the submitted data has errors"
+     *  }
+     * )
+     *
+     * @param Request $request the request object
+     *
+     * @return FormTypeInterface|View
+     */
+    public function postAction(Request $request)
+    {
+        return $this->post($request);
+    }
+
+    /**
+     * Update an existing data source entry from the submitted data or create a new ad network
+     *
+     * @ApiDoc(
+     *  section = "Data Sources Entry",
+     *  resource = true,
+     *  statusCodes = {
+     *      201 = "Returned when the resource is created",
+     *      204 = "Returned when successful",
+     *      400 = "Returned when the submitted data has errors"
+     *  }
+     * )
+     *
+     * @param Request $request the request object
+     * @param int $id the resource id
+     *
+     * @return FormTypeInterface|View
+     *
+     * @throws NotFoundHttpException when the resource does not exist
+     */
+    public function putAction(Request $request, $id)
+    {
+        return $this->put($request, $id);
+    }
+
+    /**
+     * Update an existing data source entry from the submitted data or create a new data source at a specific location
+     *
+     * @ApiDoc(
+     *  section = "Data Sources Entry",
+     *  resource = true,
+     *  statusCodes = {
+     *      204 = "Returned when successful",
+     *      400 = "Returned when the submitted data has errors"
+     *  }
+     * )
+     *
+     * @param Request $request the request object
+     * @param int $id the resource id
+     *
+     * @return FormTypeInterface|View
+     *
+     * @throws NotFoundHttpException when resource not exist
+     */
+    public function patchAction(Request $request, $id)
+    {
+        return $this->patch($request, $id);
+    }
+
+    /**
+     * Delete an existing data source entry
+     *
+     * @ApiDoc(
+     *  section = "Data Sources Entry",
+     *  resource = true,
+     *  statusCodes = {
+     *      204 = "Returned when successful",
+     *      400 = "Returned when the submitted data has errors"
+     *  }
+     * )
+     *
+     * @param int $id the resource id
+     *
+     * @return View
+     *
+     * @throws NotFoundHttpException when the resource not exist
+     */
+    public function deleteAction($id)
+    {
+        return $this->delete($id);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getResourceName()
+    {
+        return 'data_source_entry';
+    }
+
+    /**
+     * The 'get' route name to redirect to after resource creation
+     *
+     * @return string
+     */
+    protected function getGETRouteName()
+    {
+        return 'api_1_get_data_source_entry';
+    }
+
+    /**
+     * @return HandlerInterface
+     */
+    protected function getHandler()
+    {
+        return $this->container->get('tagcade_api.handler.data_source_entry');
+    }
+}
