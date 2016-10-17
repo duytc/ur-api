936a4e60518cd512f5c62651098c69988f121392
lindan999 <linhvv.tt@gmail.com>
2016-10-17 16:44:18 +0700

    adding Connected DataSource Controller and validation of mapping fields

--- src/UR/Form/Type/ConnectedDataSourceFormType.php
+++ src/UR/Form/Type/ConnectedDataSourceFormType.php
@@ -10,6 +10,7 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use UR\Entity\Core\ConnectedDataSource;
 use UR\Form\DataTransformer\RoleToUserEntityTransformer;
 use UR\Model\Core\ConnectedDataSourceInterface;
+use UR\Model\Core\DataSetInterface;
 use UR\Model\User\Role\AdminInterface;
 
 class ConnectedDataSourceFormType extends AbstractRoleSpecificFormType
@@ -21,8 +22,7 @@ class ConnectedDataSourceFormType extends AbstractRoleSpecificFormType
             ->add('dataSource')
             ->add('mapFields')
             ->add('filters')
-            ->add('transforms')
-        ;
+            ->add('transforms');
 
         if ($this->userRole instanceof AdminInterface) {
             $builder->add(
@@ -35,6 +35,16 @@ class ConnectedDataSourceFormType extends AbstractRoleSpecificFormType
         $builder->addEventListener(
             FormEvents::POST_SUBMIT,
             function (FormEvent $event) {
+                /** @var ConnectedDataSourceInterface $connDataSource */
+                $connDataSource = $event->getData();
+                $form = $event->getForm();
+
+                // validate mapping fields
+                /** @var DataSetInterface $dataSet */
+                $dataSet = $connDataSource->getDataSet();
+                if (!$this->validateMappingFields($dataSet, $connDataSource->getMapFields())) {
+                    $form->get('mapFields')->addError(new FormError('one or more fields you mapping are not exist in DataSet Dimensions or Metrics'));
+                }
             }
         );
     }
@@ -47,5 +57,17 @@ class ConnectedDataSourceFormType extends AbstractRoleSpecificFormType
     public function getName()
     {
         return 'ur_form_connected_data_source';
+    }
+
+    public function validateMappingFields(DataSetInterface $dataSet, $mapFields)
+    {
+        // check if mapping fields are exist in Data Set
+        foreach ($mapFields as $mapField) {
+            if (!array_key_exists($mapField, $dataSet->getDimensions()))
+                if (!array_key_exists($mapField, $dataSet->getMetrics())) {
+                    return false;
+                }
+        }
+        return true;
     }
 }
