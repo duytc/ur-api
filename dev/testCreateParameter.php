707707aecf1fb17d2ef47752b31140026107ce6d
duytc <tranduy8683@gmail.com>
2016-11-11 17:31:45 +0700

    add test script

--- dev/testCreateParameter.php
+++ dev/testCreateParameter.php
@@ -1,6 +1,24 @@
 <?php
+$loader = require_once __DIR__ . '/../app/autoload.php';
+require_once __DIR__ . '/../app/AppKernel.php';
 
+$kernel = new AppKernel('dev', true);
+$kernel->boot();
 
-class testCreateParameter {
+$report = [
+    ['date' => '2016-11-10', 'impressions' => 12, 'clicks' => 24, 'name' => 'tag 1','partner'],
+    ['date' => '2016-11-10', 'impressions' => 12, 'clicks' => 24, 'name' => 'tag 2'],
+    ['date' => '2016-11-10', 'impressions' => 13, 'clicks' => 25, 'name' => 'tag 1'],
+    ['date' => '2016-11-10', 'impressions' => 14, 'clicks' => 26, 'name' => 'tag 1'],
+    ['date' => '2016-11-10', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
+    ['date' => '2016-11-09', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
+    ['date' => '2016-11-09', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
+    ['date' => '2016-11-09', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 2'],
+    ['date' => '2016-11-09', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
+    ['date' => '2016-11-08', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
+    ['date' => '2016-11-08', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
+];
 
-} 
+$grouper = new \UR\Service\Report\Groupers\DefaultGrouper();
+$result = $grouper->getGroupedReport(['name'], $report, ['impressions', 'clicks'], ['date', 'name']);
+var_dump($result);
--- dev/test_grouper.php
+++ dev/test_grouper.php
@@ -6,17 +6,19 @@ $kernel = new AppKernel('dev', true);
 $kernel->boot();
 
 $report = [
-    ['date' => '2016-11-10', 'impressions' => 12, 'clicks' => 24, 'name' => 'tag 1'],
+    ['date' => '2016-11-10', 'impressions' => 12, 'clicks' => 24, 'name' => 'tag 1','partner'],
+    ['date' => '2016-11-10', 'impressions' => 12, 'clicks' => 24, 'name' => 'tag 2'],
     ['date' => '2016-11-10', 'impressions' => 13, 'clicks' => 25, 'name' => 'tag 1'],
     ['date' => '2016-11-10', 'impressions' => 14, 'clicks' => 26, 'name' => 'tag 1'],
     ['date' => '2016-11-10', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
     ['date' => '2016-11-09', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
     ['date' => '2016-11-09', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
+    ['date' => '2016-11-09', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 2'],
     ['date' => '2016-11-09', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
     ['date' => '2016-11-08', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
     ['date' => '2016-11-08', 'impressions' => 15, 'clicks' => 27, 'name' => 'tag 1'],
 ];
 
 $grouper = new \UR\Service\Report\Groupers\DefaultGrouper();
-$result = $grouper->getGroupedReport('date', $report, ['impressions', 'clicks'], ['date', 'name']);
+$result = $grouper->getGroupedReport(['name'], $report, ['impressions', 'clicks'], ['date', 'name']);
 var_dump($result);
