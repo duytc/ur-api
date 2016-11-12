f0452ea95293762942f75f8d16ab1a097adedf84
duytc <tranduy8683@gmail.com>
2016-11-12 09:18:38 +0700

    add getStartDate and getEndDate to Filters

--- src/UR/Domain/DTO/Report/Filters/DateFilterInterface.php
+++ src/UR/Domain/DTO/Report/Filters/DateFilterInterface.php
@@ -10,5 +10,14 @@ interface DateFilterInterface extends AbstractFilterInterface
      * @return string
      */
     public function getDateFormat();
+    /**
+     * @return mixed
+     */
+    public function getEndDate();
+
+    /**
+     * @return array
+     */
+    public function getStartDate();
 
 }
