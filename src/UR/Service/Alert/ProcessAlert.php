380390b22745f362815ddfed5d3a4d0680d161f1
duytc <tranduy8683@gmail.com>
2016-11-14 11:37:18 +0700

    refactor alert interface

--- src/UR/Service/Alert/ProcessAlert.php
+++ src/UR/Service/Alert/ProcessAlert.php
@@ -5,27 +5,23 @@ namespace UR\Service\Alert;
 
 use UR\Bundle\UserBundle\DomainManager\PublisherManagerInterface;
 use UR\DomainManager\AlertManagerInterface;
-use UR\DomainManager\ConnectedDataSourceManagerInterface;
-use UR\DomainManager\DataSourceEntryManagerInterface;
-use UR\Entity\Core\Alert;
 
 class ProcessAlert implements ProcessAlertInterface
 {
     protected $alertManager;
-    protected $dataSourceEntryManager;
-    protected $connectedDataSource;
     protected $publisherManager;
-    protected  $alertCodes;
+    protected $alertCodes;
 
-    public function __construct($alertCodes, AlertManagerInterface $alertManager, DataSourceEntryManagerInterface $dataSourceEntryManager, ConnectedDataSourceManagerInterface $connectedDataSource, PublisherManagerInterface $publisherManager)
+    public function __construct($alertCodes, AlertManagerInterface $alertManager, PublisherManagerInterface $publisherManager)
     {
-        $this->alertCodes =  $alertCodes;
+        $this->alertCodes = $alertCodes;
         $this->alertManager = $alertManager;
-        $this->dataSourceEntryManager = $dataSourceEntryManager;
-        $this->connectedDataSource = $connectedDataSource;
         $this->publisherManager = $publisherManager;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function createAlert($alertCode, $publisherId, $messageDetail)
     {
 
--- src/UR/Service/Alert/ProcessAlertInterface.php
+++ src/UR/Service/Alert/ProcessAlertInterface.php
@@ -5,6 +5,12 @@ namespace UR\Service\Alert;
 
 interface ProcessAlertInterface
 {
+    /**
+     * @param $alertCode
+     * @param $publisherId
+     * @param $messageDetail
+     * @return mixed
+     */
     public function createAlert($alertCode, $publisherId, $messageDetail);
 
 }
