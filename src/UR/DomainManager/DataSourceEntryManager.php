4a03fb69f01777898be4748a3d990310ad6b2ba7
cuong <=>
2016-11-18 16:02:03 +0700

    Update to detected insensitive fields

--- src/UR/DomainManager/DataSourceEntryManager.php
+++ src/UR/DomainManager/DataSourceEntryManager.php
@@ -221,17 +221,17 @@ class DataSourceEntryManager implements DataSourceEntryManagerInterface
     {
         switch ($option) {
             case DataSourceEntryManager::UPLOAD:
-                if (!array_key_exists($newField, $detectedFields)) {
-                    $detectedFields[$newField] = 1;
+                if (!array_key_exists(strtolower(trim($newField)), $detectedFields)) {
+                    $detectedFields[strtolower(trim($newField))] = 1;
                 } else {
-                    $detectedFields[$newField] += 1;
+                    $detectedFields[strtolower(trim($newField))] += 1;
                 }
                 break;
             case DataSourceEntryManager::DELETE:
-                if (isset($detectedFields[$newField])) {
-                    $detectedFields[$newField] -= 1;
-                    if ($detectedFields[$newField] <= 0) {
-                        unset($detectedFields[$newField]);
+                if (isset($detectedFields[strtolower(trim($newField))])) {
+                    $detectedFields[strtolower(trim($newField))] -= 1;
+                    if ($detectedFields[strtolower(trim($newField))] <= 0) {
+                        unset($detectedFields[strtolower(trim($newField))]);
                     }
                 }
                 break;
