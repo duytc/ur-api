4099b701801d3c991997c9e01769669e11af867b
lindan999 <linhvv.tt@gmail.com>
2017-02-03 16:44:10 +0700

    command for adding unique_id to data import table if not exist before

---
+++ src/UR/Bundle/AppBundle/Command/AddUniqueColumnCommand.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace UR\Bundle\AppBundle\Command;
+
+use Doctrine\DBAL\Schema\Comparator;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\TableDiff;
+use Symfony\Bridge\Monolog\Logger;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use UR\DomainManager\IntegrationManagerInterface;
+use UR\Model\Core\DataSetInterface;
+use UR\Service\DataSet\Importer;
+use UR\Service\DataSet\Locator;
+use UR\Service\DataSet\Synchronizer;
+use UR\Service\DataSet\Type;
+use UR\Service\StringUtilTrait;
+
+class AddUniqueColumnCommand extends ContainerAwareCommand
+{
+    use StringUtilTrait;
+
+    /** @var Logger */
+    private $logger;
+
+    protected function configure()
+    {
+        $this
+            ->setName('ur:import-table:add-unique-column')
+            ->setDescription('Add column __unique_id for data import table if not exist');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var ContainerInterface $container */
+        $container = $this->getContainer();
+        /** @var Logger $logger */
+        $this->logger = $container->get('logger');
+
+        $this->logger->info('starting command...');
+
+        /** @var IntegrationManagerInterface $integrationManager */
+        $dataSetManager = $container->get('ur.domain_manager.data_set');
+
+        /** @var DataSetInterface[] $dataSets */
+        $dataSets = $dataSetManager->all();
+
+        $em = $container->get('doctrine.orm.entity_manager');
+        $conn = $em->getConnection();
+        $schema = new Schema();
+        $dataSetLocator = new Locator($conn);
+        $dataSetSynchronizer = new Synchronizer($conn, new Comparator());
+
+        /** @var DataSetInterface[] $dataSetMissingUniques */
+        $dataSetMissingUniques = [];
+        foreach ($dataSets as $dataSet) {
+            $dataTable = $dataSetLocator->getDataSetImportTable($dataSet->getId());
+            if (!$dataTable) {
+                continue;
+            }
+
+            if ($dataTable->hasColumn(DataSetInterface::UNIQUE_ID_COLUMN)) {
+                continue;
+            }
+
+            $dataSetMissingUniques[] = $dataSet;
+        }
+
+        foreach ($dataSetMissingUniques as $dataSetMissingUnique) {
+            $dataSetTable = $dataSetLocator->getDataSetImportTable($dataSetMissingUnique->getId());
+            $addCols = [];
+            $addCols[] = $dataSetTable->addColumn(DataSetInterface::UNIQUE_ID_COLUMN, Type::TEXT, array("notnull" => true));
+
+            $updateTable = new TableDiff($dataSetTable->getName(), $addCols, [], [], [], [], []);
+
+            try {
+                $dataSetSynchronizer->syncSchema($schema);
+                $alterSqls = $conn->getDatabasePlatform()->getAlterTableSQL($updateTable);
+                foreach ($alterSqls as $alterSql) {
+                    $conn->exec($alterSql);
+                }
+            } catch (\Exception $e) {
+                $logger->error("Cannot Sync Schema " . $schema->getName());
+            }
+
+            $dimensions = array_keys($dataSetMissingUnique->getDimensions());
+            $qb = $conn->createQueryBuilder();
+
+            foreach ($dimensions as $dimension) {
+                $qb->addSelect($dimension);
+                $qb->addSelect(Importer::ID_COLUMN);
+            }
+
+            $qb->from($dataSetTable->getName());
+            $rows = $qb->execute()->fetchAll();
+
+            foreach ($rows as $row) {
+                $id = $row[Importer::ID_COLUMN];
+                unset($row[Importer::ID_COLUMN]);
+
+                foreach ($row as $k => $item) {
+                    if ($item === null) {
+                        unset($row[$k]);
+                    }
+                }
+
+                $uniqueId = md5(implode(":", $row));
+
+                $updateQb = $conn->createQueryBuilder();
+                $updateQb->update($dataSetTable->getName(), 't')->set(DataSetInterface::UNIQUE_ID_COLUMN, "'" . $uniqueId . "'");
+                $updateQb->where('t.__id=:table_id')->setParameter(':table_id', $id);
+                $updateQb->execute();
+            }
+        }
+
+        $this->logger->info(sprintf('command run successfully'));
+    }
+}
