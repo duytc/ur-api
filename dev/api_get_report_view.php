3c620c834a4dab6819022fad3be825c6db65d98d
gfreeau <greg@imagize.com.au>
2017-09-26 12:07:20 -0400

    Improve API example to get a report view

--- dev/api_get_report_view.php
+++ dev/api_get_report_view.php
@@ -1,17 +1,20 @@
 <?php
-$username = 'admin';
-$password = '123456';
-$reportViewId = 4;
-$getTokenUrl = 'http://api.unified-reports.dev/app_dev.php/api/v1/getToken';
-$getAccountReportUrl = 'http://api.unified-reports.dev/app_dev.php/api/v1/reportview/' . intval($reportViewId) . '/reportResult';
+$username = 'uruser';
+$password = 'abc123';
+$reportViewId = 1;
+$getTokenUrl = 'http://api.tagcade.dev/app_dev.php/api/v1/getToken';
+$getReportViewUrl = 'http://api.unified-reports.dev/app_dev.php/api/v1/reportview/'.intval($reportViewId).'/reportResult';
 
-
-// Step 1. Get token
+// Step 1: Get token
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $getTokenUrl);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData = array('username'=>$username, 'password'=>$password)));
+curl_setopt(
+    $ch,
+    CURLOPT_POSTFIELDS,
+    http_build_query($postData = array('username' => $username, 'password' => $password))
+);
 
 $response = curl_exec($ch);
 curl_close($ch);
@@ -26,40 +29,48 @@ if (!array_key_exists('token', $token) || !array_key_exists('id', $token)) {
     throw new RuntimeException('Invalid output');
 }
 
-var_dump($token);
-
 $publisherId = $token['id'];
 $token = $token['token'];
 
-// Step 2. Do request to api (in this example we do get account report
+// Step 2: Define API payload
+$data = [
+//    "startDate" => "2016-01-01",
+//    "endDate" => "2017-10-11",
+//    "searches" =>
+//        [
+//            "report_view_alias" => "re"
+//        ],
+    "limit" => 10,
+    "page" => 1,
+    "userDefineDimensions" =>
+    [
+        "date_1",
+        "demand_partner_1",
+        "partner_tag_id_1",
+    ],
+    "userDefineMetrics" =>
+    [
+        "rev_share_percent",
+        "cpm",
+        "rev_share",
+        "impressions_1",
+        "revenue_1",
+    ],
+];
+
+$jsonData = json_encode($data);
+
+// Step 3: Do request to api
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $header = array("Authorization: Bearer $token"));
-curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-
-$queryParams = [
-    "startDate" => "2016-01-01",
-    "endDate" => "2017-10-11",
-    "searches" =>
-        [
-            "report_view_alias" => "re"
-        ],
-    "limit" =>10,
-    "page" =>1,
-    "userDefineDimensions" =>
-        [
-            "report_view_alias"
-        ],
-    "userDefineMetrics" =>
-        [
-            "ad_impression_1",
-            "revenue_1"
-        ]
-];
-
-$fullUrl = sprintf('%s?%s', $getAccountReportUrl, http_build_query($queryParams));
-curl_setopt($ch, CURLOPT_URL, $fullUrl);
+curl_setopt($ch, CURLOPT_URL, $getReportViewUrl);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Authorization: Bearer $token",
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($jsonData),
+]);
 
 $reports = curl_exec($ch);
 curl_close($ch);
