0a9f2c0db617ce66616f9c97d056a48de8b3b3a9
dtagnhanvv <dtag.nhanvv@gmail.com>
2017-10-10 09:02:20 +0700

    Hot fix: Pagination fail for View Loaded Data

--- src/UR/Repository/Core/ImportHistoryRepository.php
+++ src/UR/Repository/Core/ImportHistoryRepository.php
@@ -75,11 +75,6 @@ class ImportHistoryRepository extends EntityRepository implements ImportHistoryR
                     $qb->expr()->like('ih.id', ':searchKey')
                 ))
                 ->setParameter('searchKey', $searchLike);
-
-            $searchLike = sprintf('%%%s%%', str_replace("/", "-", $param->getSearchKey()));
-            $qb
-                ->orWhere($qb->expr()->like('ih.createdDate', ':date'))
-                ->setParameter('date', $searchLike);
         }
 
         if (is_string($param->getSortField()) &&
