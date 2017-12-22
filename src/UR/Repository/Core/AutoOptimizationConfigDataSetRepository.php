6150c489a390b10bf96e5531159e12275bbb81db
jennyphuong <phuongdinh5678@gmail.com>
2017-12-22 16:10:56 +0700

    update can not delete dataset which dataset has autoOptimizationConfig

--- src/UR/Repository/Core/AutoOptimizationConfigDataSetRepository.php
+++ src/UR/Repository/Core/AutoOptimizationConfigDataSetRepository.php
@@ -5,8 +5,19 @@ namespace UR\Repository\Core;
 
 
 use Doctrine\ORM\EntityRepository;
+use UR\Model\Core\DataSetInterface;
 
 class AutoOptimizationConfigDataSetRepository extends EntityRepository implements AutoOptimizationConfigDataSetRepositoryInterface
 {
-
+    /**
+     * @inheritdoc
+     */
+    public function findByDataSet(DataSetInterface $dataSet)
+    {
+        return $this->createQueryBuilder('aocds')
+            ->where('aocds.dataSet = :dataSet')
+            ->setParameter('dataSet', $dataSet)
+            ->getQuery()
+            ->getResult();
+    }
 }
--- src/UR/Repository/Core/AutoOptimizationConfigDataSetRepositoryInterface.php
+++ src/UR/Repository/Core/AutoOptimizationConfigDataSetRepositoryInterface.php
@@ -5,8 +5,13 @@ namespace UR\Repository\Core;
 
 
 use Doctrine\Common\Persistence\ObjectRepository;
+use UR\Model\Core\DataSetInterface;
 
 interface AutoOptimizationConfigDataSetRepositoryInterface extends ObjectRepository
 {
-
+    /**
+     * @param DataSetInterface $dataSet
+     * @return mixed
+     */
+    public function findByDataSet(DataSetInterface $dataSet);
 }
